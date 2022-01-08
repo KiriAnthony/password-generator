897ce250eb384a239d76439377c637e6e3f5ab1e
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-01-08 11:16:54 -0700

    adding to script.js outline

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Function to generate and pass password to webpage
+function generatePassword() {
+// Asking user input to determine characters in password
+
+// Using arrays and random numbers to generate a password through for loop
+
+}
